9fc7db835965a7d8fe3ccb6c1c1186efce9989c7
Rysmith526 <rmatthew.smith@ufl.edu>
2023-03-09 11:57:08 -0500

    Added decoder function

--- encoder.py
+++ encoder.py
@@ -4,7 +4,12 @@ def encoder(password):
         encoded_pass = encoded_pass + str((int(char) + 3) % 10)
     return encoded_pass
 
+
 def decoder(password):
+    decoded_pass = ""
+    for char in password:
+        decoded_pass = decoded_pass + str((int(char) + 7) % 10)
+    return decoded_pass
 
 
 def main():
