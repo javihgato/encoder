7cd6ec6c239a5c530eabf7541e32d350f677e1ea
javihgato <127428467+javihgato@users.noreply.github.com>
2023-03-09 11:54:27 -0500

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -9,10 +9,11 @@ def decode(password):
 
 
 def main():
+    password = 0
     while True:
-        print("1. To encode")
-        print("2. To decode")
-        print("3. To exit")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
         choice = input("Please enter an option: ")
         if choice == 1:
             password = encode(input('Please enter your password to encode: '))
