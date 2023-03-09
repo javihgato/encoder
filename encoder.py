95083f1ff68cc79cb821887ad7a3648e449409cd
javihgato <127428467+javihgato@users.noreply.github.com>
2023-03-09 12:11:47 -0500

    Add files via upload
    Fixed a bug with user input selection

--- encoder.py
+++ encoder.py
@@ -12,13 +12,14 @@ def decode(password):
         decoded_pass = decoded_pass + str((int(char) + 7) % 10)
     return decoded_pass
 
+
 def main():
     password = 0
     while True:
         print("1. Encode")
         print("2. Decode")
         print("3. Quit")
-        choice = input("Please enter an option: ")
+        choice = str(input("Please enter an option: "))
         if choice == 1:
             password = encode(input('Please enter your password to encode: '))
             print('Your password has been encoded and stored!')
