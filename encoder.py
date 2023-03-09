b83022a630406fecb3413fff92abd313456cb7a2
javihgato <127428467+javihgato@users.noreply.github.com>
2023-03-09 11:36:39 -0500

    Add files via upload

--- encoder.py
+++ encoder.py
@@ -1,5 +1,28 @@
-def Encoder(password):
+def encoder(password):
     encoded_pass = ""
     for char in password:
         encoded_pass = encoded_pass + str((int(char) + 3) % 10)
     return encoded_pass
+
+def decoder(password):
+
+
+def main():
+    while True:
+        print("1. To encode")
+        print("2. To decode")
+        print("3. To exit")
+        choice = input("Please enter an option: ")
+        if choice == "1":
+            password = input("Enter an 8-digit password: ")
+            print("Encoded password is", encoder(password))
+        elif choice == "2":
+            password = input("Enter an already encoded 8-digit password: ")
+            print("Decoded password is", decoder(password))
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice")
+        print()
+
+main()
